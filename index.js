65ae4ec68783a95b0b0ea5428118f6f7f47a36b9
Melanie Brownfield <melanie.brownfield1@gmail.com>
2018-01-18 22:23:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ function sayHiToGrandma(string) {
 } else if (string.toUpperCase() === string)
 return "YES INDEED!" }
 
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
 
 
 
